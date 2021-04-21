012c8f44b2eb0f854f3e7965a3f21949c162aec6
Chace Ashcraft <Chace.Ashcraft@jhuapl.edu>
2021-04-21 13:34:49 -0600

    Adding first structural similarity algorithm and Wang paper results replication code.

---
+++ structural_similarity.py
@@ -0,0 +1,112 @@
+import numpy as np
+import ot
+
+
+def directed_hausdorff_numpy(delta_a, N_u, N_v):
+    """
+    Directed Hausdorff distance using a distance matrix and the out-neighbors of state nodes u and v. Note the the full
+        Hausdorff distance is given by:
+
+        max(directed_hausdorff_numpy(delta_a, N_u, N_v), directed_hausdorff_numpy(delta_a, N_v, N_u))
+
+    :param delta_a: (np.array) State distance matrix.
+    :param N_u: (list or np.array) Out neighbors (action nodes) of state node u.
+    :param N_v: (list or np.array) Out neighbors (action nodes) of state node v.
+    :return: (float) Directed Hausdorff distance.
+    """
+    return delta_a[np.array(N_u)].swapaxes(0, 1)[np.array(N_v)].swapaxes(0, 1).min(axis=1).max()
+
+
+def structural_similarity(action_dists, reward_matrix, out_neighbors_S, c_a=0.95, c_s=0.95, stop_rtol=1e-5,
+                          stop_atol=1e-8, max_iters=1e5):
+    """
+    Compute the structural similarity of an MDP graph as inspired by Wang et. al. paper:
+        https://www.ijcai.org/Proceedings/2019/0511.pdf.
+        Note: We still want to augment this to work for different numbers of states/actions for different MDPs
+    :param action_dists: (np.array) P(s' | s, a) for each action node in the MDP graph.
+    :param reward_matrix: (np.array) r(s, a) for each action node in the MDP graph.
+    :param out_neighbors_S: (dict) Dictionary mapping the state nodes to their corresponding action nodes.
+    :param c_a: (float) a constant in [0, 1] discounting the impact of the neighbors Nα and Nβ on the pair of state
+        nodes (u, v). Typically set to 0.95.
+    :param c_s: (float) parameter in [0, 1] to weight the importance of the reward similarity and the transition
+        similarity. Typically set to 0.95.
+    :param stop_rtol: (float) The rtol paramter to numpy.allclose. Used for convergence criteria.
+    :param stop_atol: (float) The atol paramter to numpy.allclose. Used for convergence criteria.
+    :param max_iters: (int) Maximum number of iterations to attempt to make distance matrices converge.
+    :return: (np.array, np.array, int, bool) (S_star, A_star, num_iters, done) The state and action similarity matrices,
+        respectively, then then number of iterations to convergence and a bool stating whether it actually converged or
+        not.
+    """
+
+    n_actions, n_states = action_dists.shape
+
+    S = np.eye(n_states)
+    A = np.eye(n_actions)
+    states = list(range(n_states))
+
+    one_minus_c_a = 1 - c_a  # optimization
+
+    last_S = S.copy()
+    last_A = A.copy()
+
+    done = False
+    iter = 0
+    while not done and iter < max_iters:
+        for u in states:
+            for v in states[u + 1:]:
+                # Note: Could we just use the P matrix to know what the out_neighbors are for actions and states?
+                N_u = out_neighbors_S[u]
+                N_v = out_neighbors_S[v]
+                for alpha in N_u:
+                    for beta in N_v:
+                        p_alpha = action_dists[alpha]
+                        p_beta = action_dists[beta]
+                        d_rwd = abs(np.sum(reward_matrix[alpha] * p_alpha) - np.sum(reward_matrix[beta] * p_beta))
+                        emd = ot.emd2(p_alpha, p_beta, 1 - S)  # Note: can be >1 by small rounding error
+                        entry = 1 - one_minus_c_a * d_rwd - c_a * emd
+                        A[alpha, beta] = entry
+                        A[beta, alpha] = entry  # have to keep track of whole matrix for directed hausdorff
+
+        for u in states:
+            for v in states[u + 1:]:
+                if len(out_neighbors_S[u]) == 0 or len(out_neighbors_S[v]) == 0:
+                    continue  # skip
+                haus = max(directed_hausdorff_numpy(1 - A, out_neighbors_S[u], out_neighbors_S[v]),
+                           directed_hausdorff_numpy(1 - A, out_neighbors_S[v], out_neighbors_S[u]))
+                entry = c_s * (1 - haus)
+                S[u, v] = entry
+                S[v, u] = entry  # Note: might be unnecessary, just need upper triangle of matrix due to symmetry
+
+        if np.allclose(A, last_A, rtol=stop_rtol, atol=stop_atol) and np.allclose(S, last_S, rtol=stop_rtol,
+                                                                                  atol=stop_atol):
+            # Note: Could update this to use specified more specific convergence criteria
+            done = True
+        else:
+            last_S = S.copy()
+            last_A = A.copy()
+
+        iter += 1
+
+    return S, A, iter - 1, done  # return done to report that it converged (or didn't)
+
+
+if __name__ == "__main__":
+    # Example based on the MDP described in Figure 1 of https://www.ijcai.org/Proceedings/2019/0511.pdf.
+    out_neighbors_S = {0: [0, 1], 1: [2, 3], 2: [], 3: [], 4: [], 5: []}
+    P = np.array([[0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.0, 0.2, 0.5, 0.3],
+                  [0.0, 0.0, 1.0, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.15, 0.15, 0.25, 0.45]])
+    R = np.array([[0.0, 0.0, 0.3, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.0, 0.6, 0.7, 0.9],
+                  [0.0, 0.0, 0.1, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.2, 0.4, 0.6, 0.5]])
+
+    c_s = 0.95
+    c_a = 0.95
+    s, a, num_iters, d = structural_similarity(P, R, out_neighbors_S, c_a, c_s)
+
+    print("S\n", s)
+    print("A\n", a)
+    print("num iters:", num_iters)
+    print("converged:", d)
---
+++ wang_paper_experiment.py
@@ -0,0 +1,116 @@
+"""
+Code to replicate the results from Wang paper (https://www.ijcai.org/Proceedings/2019/0511.pdf), section 5.1.
+"""
+
+import numpy as np
+from structural_similarity import structural_similarity
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# grid: where goal state == 4
+# 6 7 8
+# 3 4 5
+# 0 1 2
+
+a_map = {"left": 0, "right": 1, "up": 2, "down": 3}
+
+
+def move_allowed(grid_shape, state, action):
+    # if a move is allowed (has positive probability) for Wang paper example.
+    if state == 4:  # goal state
+        return False, None
+    if action == "left":
+        if state % grid_shape[1] == 0:
+            return False, None
+        else:
+            s_p = state - 1
+            return True, s_p
+    elif action == "right":
+        if state % grid_shape[1] == grid_shape[0] - 1:
+            return False, None
+        else:
+            s_p = state + 1
+            return True, s_p
+    elif action == "up":
+        if state // grid_shape[1] % grid_shape[0] == grid_shape[0] - 1:
+            return False, None
+        else:
+            s_p = state + grid_shape[1]
+            return True, s_p
+    elif action == "down":
+        if state < grid_shape[1]:
+            return False, None
+        else:
+            s_p = state - grid_shape[1]
+            return True, s_p
+
+
+if __name__ == "__main__":
+    P = np.zeros((9, 4))
+    out_neighbors_s = dict(zip(range(9), [list() for i in range(9)]))
+    out_neighbors_a = dict()
+
+    a_node = 0
+    for s in range(9):
+        for act in list(a_map.keys()):
+            move_allow, s_prime = move_allowed((3, 3), s, act)
+            if move_allow:  # add action node
+                out_neighbors_s[s].append(a_node)
+                out_neighbors_a[a_node] = np.zeros(9)
+                for a_p in list(a_map.keys()):
+                    if act == a_p:  # agent can go in the given direction
+                        out_neighbors_a[a_node][s_prime] = 0.9
+                        continue
+                    allowed2, s_prime2 = move_allowed((3, 3), s, a_p)
+                    if allowed2:  # agent can go in alternate direction as well
+                        out_neighbors_a[a_node][s_prime2] = 0.1
+
+                # If there are multiple alternate states besides the chosen action, normalize the probabilities between
+                #   them to get a correct probability distribution
+                if np.sum(out_neighbors_a[a_node]) > 1.0:
+                    eq_01 = np.where(out_neighbors_a[a_node] == 0.1)
+                    n_eq = len(eq_01[0])
+                    out_neighbors_a[a_node][np.where(out_neighbors_a[a_node] == 0.1)] = 0.1 / n_eq
+                a_node += 1
+
+    P = np.array([arr for arr in out_neighbors_a.values()])
+    R = np.zeros((20, 9))
+    for i in range(20):
+        if P[i, 4] > 0:
+            R[i, 4] = 1.0
+
+    print("State out neighbors dict:")
+    print(out_neighbors_s)
+    print("Action out neighbors dict:")
+    print(out_neighbors_a)
+    print("Action node transition probabilities:")
+    print(P)
+    print("Action node reward vectors:")
+    print(R)
+
+    # compute structural similarity
+    sigma_s, sigma_a, num_iters, done = structural_similarity(P, R, out_neighbors_s)
+
+    print("delta_S:")
+    print(1 - sigma_s)
+
+    print("Exact comparison value from paper:")
+    print(f"delta_S(0, 2): Ours: {1 - sigma_s[0, 2]}, Paper: 0.27489")
+    print(f"delta_S(0, 6): Ours: {1 - sigma_s[0, 6]}, Paper: 0.27489")
+    print(f"delta_S(1, 3): Ours: {1 - sigma_s[1, 3]}, Paper: 0.27492")
+    print(f"delta_S(1, 5): Ours: {1 - sigma_s[1, 5]}, Paper: 0.27492")
+    print(f"delta_S(1, 7): Ours: {1 - sigma_s[1, 7]}, Paper: 0.28627")
+    print(f"delta_S(0, 8): Ours: {1 - sigma_s[0, 8]}, Paper: 0.29873")
+
+    print("Iterations to converge:", num_iters)
+
+    upper = np.triu(sigma_s)
+
+    ax = sns.heatmap(1 - upper, linewidth=0.5, cmap="rainbow")
+    plt.show()
+
+
+
+
+
+
