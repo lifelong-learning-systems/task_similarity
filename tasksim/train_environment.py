f0f1b2d93a1fe948246283394aa31ad843f5db57
Ben Stoler <Benjamin.Stoler@jhuapl.edu>
2021-07-27 13:54:51 -0400

    made test/train options

--- tasksim/train_environment.py
+++ tasksim/train_environment.py
@@ -7,36 +7,68 @@ import tasksim
 from tasksim.environment import MDPGraphEnv
 from ray.tune.schedulers import PopulationBasedTraining
 import tasksim.gridworld_generator as gen
+import numpy as np
 
+import argparse
+import sys
+
+
+
+RANDOM_SEED = 41239678
+RANDOM_STATE = np.random.RandomState(RANDOM_SEED)
+GRID = gen.create_grid((10, 10), obstacle_prob=0.2, random_state=RANDOM_STATE)
+G = gen.MDPGraph.from_grid(GRID, strat=gen.ActionStrategy.WRAP_NOOP_EFFECT)
+ENV = MDPGraphEnv(G)
 
 def env_creator(_):
-    grid = gen.create_grid((10, 10), obstacle_prob=0.2)
-    G = gen.MDPGraph.from_grid(grid, strat=gen.ActionStrategy.WRAP_NOOP_EFFECT)
-    env = MDPGraphEnv(G)
-    return env # return an env instance
+    return ENV # return an env instance
 
-register_env("gridworld", env_creator)
+# TODO:
+# - small negative rewards, when reward is 0?
+# - random variation/noise of transition probs
 
-config = ppo.DEFAULT_CONFIG.copy()
-print(config)
-config.update({"env": "gridworld",
-        'env_config':{'visualize':False},
-        # "callbacks": {
-        #     "on_train_result": on_train_result,
-        # },
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test', help='test specified checkpoint')
+    parser.add_argument('--iters', default=100, help='number of iters to train')
+    args = parser.parse_args()
+    train = args.test is None
+    iters = int(args.iters)
+    register_env("gridworld", env_creator)
 
-        'framework':'torch',
-        "num_workers": 1,
-        'model': {
-                "use_lstm": False,
+    config = ppo.DEFAULT_CONFIG.copy()
+    print(config)
+    config.update({"env": "gridworld",
+            'env_config':{'visualize':False},
+            # "callbacks": {
+            #     "on_train_result": on_train_result,
+            # },
+
+            'framework':'torch',
+            "num_workers": 4,
+            'model': {
+                    "use_lstm": False,
             }
-        })
+            })
 
 
+    if train:
+        tune.run(PPOTrainer, config=config,
+                checkpoint_freq = 1,
+                name="gridworld",
+                stop={'training_iteration': iters}
+        ) 
+    else:
+        ray.init()
+        agent = ppo.PPOTrainer(config=config, env='gridworld')
+        agent.restore(args.test)
 
-tune.run(PPOTrainer, config=config,
-        checkpoint_freq = 1,
-        name="gridworld",
-)  # "log_level": "INFO" for verbose,
-                                                     # "framework": "tfe"/"tf2" for eager,
+        import code; code.interact(local=vars())
+        done = False
+        ep_reward = 0
+        obs = ENV.reset()
+        while not done:
+                action = agent.compute_action(obs)
+                obs, reward, done, _ = ENV.step(action)
+                ep_reward += reward
 
