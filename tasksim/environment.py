840d2d20d5d5f28e97ccb9071700254d14c3df5e
Ben Stoler <Benjamin.Stoler@jhuapl.edu>
2021-07-30 16:34:04 -0400

    limit observation space attempt 1

--- tasksim/environment.py
+++ tasksim/environment.py
@@ -10,20 +10,32 @@ import tasksim.structural_similarity as sim
 
 class MDPGraphEnv(gym.Env):
 
-    def __init__(self, graph: gen.MDPGraph, random_state=None):
+    # TODO: how does POMDP impact the metric?
+    def __init__(self, graph: gen.MDPGraph, obs_size=7, random_state=None):
         super(MDPGraphEnv, self).__init__()
         self.random_state = random_state if random_state is not None else np.random
         supported_strats = [gen.ActionStrategy.NOOP_EFFECT, gen.ActionStrategy.WRAP_NOOP_EFFECT]
         # TODO: change from assertion to throw? Or just don't take graph instance; take grid, prob, etc. instead
         assert graph.strat in supported_strats, f'Unsupported ActionStrategy: {graph.strat}'
+        assert obs_size >= 3 and obs_size % 2 == 1, f'Obs Size must be an odd number >= 3'
         self.graph = graph
 
         self.action_space = spaces.Discrete(4)
+        self.obs_size = obs_size
         self.grid = graph.grid
         self.height, self.width = self.grid.shape
-        # TODO: change dtype?
-        # 0 = normal, 1 = obstacle, 2 = goal state, 3 = current location
-        self.observation_space = spaces.Box(low=0, high=3, shape=(self.height, self.width), dtype=np.uint8)
+        # TODO: change dtype
+        # 0 = normal, 1 = obstacle, 2 = goal state, 3 = current location, 4 = inaccessible (if not wraparound)/not visible
+        self.observation_space = spaces.Box(low=0, high=3, shape=(obs_size, obs_size), dtype=np.uint8)
+        # e.g. simple 3x3, upper-left:
+        # 4 4 4
+        # 4 3 0
+        # 4 0 0
+
+        # then, after moving left and wrapping around
+        # 4 4 4
+        # 0 3 4
+        # 0 0 4
 
     # Where do we start on the grid? Random location? Upper-left? Parameter of reset()?
     # -> effectively an epsilon transition from an augmented start state node into the rest of the MDP Graph
@@ -41,10 +53,30 @@ class MDPGraphEnv(gym.Env):
         return self.gen_obs()
 
     # TODO: should observation be whole grid? or just a local snapshot, etc.
+    # TODO: limited observation size
+    # TODO: limited observation lag behind/around (like in Mario) or agent always center but "black out" other squares? Or see wrap-around (if available)
     def gen_obs(self):
-        base_grid = self.grid.copy()
+        # base_grid = self.grid.copy()
+        # row, col = self.row_col(self.state)
+        # base_grid[row, col] = 3
+        # return base_grid
         row, col = self.row_col(self.state)
-        base_grid[row, col] = 3
+        base_grid = 4*np.ones((self.obs_size, self.obs_size))
+        outward = self.obs_size // 2
+        height, width = self.grid.shape
+        lim_left = max(0, col - outward)
+        lim_right = min(width - 1, col + outward)
+        lim_up = max(0, row - outward)
+        lim_down = min(height - 1, row + outward)
+        vals = self.grid[lim_up:lim_down+1, lim_left:lim_right+1]
+        # e.g. row, col = (2, 3)
+        # outward = 3
+        # lim_left = 0, lim_right = 6
+        # lim_up = 0, lim_down = 5
+        delta_left, delta_right = lim_left - col, lim_right - col
+        delta_up, delta_down = lim_up - row, lim_down - row
+        base_grid[outward+delta_up:outward+delta_down+1, outward+delta_left:outward+delta_right+1] = vals
+        base_grid[outward, outward] = 3
         return base_grid
 
     def step(self, action):
