c6285280c9c506fcea624c4b6fd0419ba006fecc
Ben Stoler <Benjamin.Stoler@jhuapl.edu>
2021-08-04 17:32:16 -0400

    added builder-pattern environment helper

--- tasksim/environment.py
+++ tasksim/environment.py
@@ -24,9 +24,9 @@ class MDPGraphEnv(gym.Env):
         self.obs_size = obs_size
         self.grid = graph.grid
         self.height, self.width = self.grid.shape
-        # TODO: change dtype
+        # TODO: stack of past N frames? like in arcade type enviros
         # 0 = normal, 1 = obstacle, 2 = goal state, 3 = current location, 4 = inaccessible (if not wraparound)/not visible
-        self.observation_space = spaces.Box(low=0, high=3, shape=(obs_size, obs_size), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=4, shape=(obs_size, obs_size), dtype=np.uint8)
         # e.g. simple 3x3, upper-left:
         # 4 4 4
         # 4 3 0
@@ -62,21 +62,21 @@ class MDPGraphEnv(gym.Env):
         # return base_grid
         row, col = self.row_col(self.state)
         base_grid = 4*np.ones((self.obs_size, self.obs_size))
-        outward = self.obs_size // 2
+        center = self.obs_size // 2
         height, width = self.grid.shape
-        lim_left = max(0, col - outward)
-        lim_right = min(width - 1, col + outward)
-        lim_up = max(0, row - outward)
-        lim_down = min(height - 1, row + outward)
+        lim_left = max(0, col - center)
+        lim_right = min(width - 1, col + center)
+        lim_up = max(0, row - center)
+        lim_down = min(height - 1, row + center)
         vals = self.grid[lim_up:lim_down+1, lim_left:lim_right+1]
         # e.g. row, col = (2, 3)
-        # outward = 3
+        # center = 3
         # lim_left = 0, lim_right = 6
         # lim_up = 0, lim_down = 5
         delta_left, delta_right = lim_left - col, lim_right - col
         delta_up, delta_down = lim_up - row, lim_down - row
-        base_grid[outward+delta_up:outward+delta_down+1, outward+delta_left:outward+delta_right+1] = vals
-        base_grid[outward, outward] = 3
+        base_grid[center+delta_up:center+delta_down+1, center+delta_left:center+delta_right+1] = vals
+        base_grid[center, center] = 3
         return base_grid
 
     def step(self, action):
--- tasksim/train_environment.py
+++ tasksim/train_environment.py
@@ -10,22 +10,151 @@ from tasksim.environment import MDPGraphEnv
 from ray.tune.schedulers import PopulationBasedTraining
 import tasksim.gridworld_generator as gen
 import numpy as np
+from sklearn.preprocessing import normalize as sk_norm
 
 import argparse
+import os
 import sys
 
 
+class EnvironmentBuilder:
+    def __init__(self, shape):
+        self.shape = shape
+        rows, cols = shape
+        self.grid = np.zeros((rows, cols))
+        center_row, center_col = rows//2, cols//2
+        self.goal_locations = [self.flatten(center_row, center_col)]
+        self.success_prob = 0.9
+        self.transition_noise = 0
+        self.obstacle_locations = []
+        self.step_reward = 0
+        self.strat = gen.ActionStrategy.WRAP_NOOP_EFFECT
+        self.obs_size = 7
+    
+    def set_obs_size(self, obs_size):
+        self.obs_size = obs_size
+        return self
+    def set_strat(self, strat):
+        self.strat = strat
+        return self
+    def set_transition_noise(self, transition_noise):
+        self.transition_noise = transition_noise
+        return self
+    def set_success_prob(self, success_prob):
+        self.success_prob = success_prob
+        return self
+    def set_step_reward(self, step_reward):
+        self.step_reward = step_reward
+        return self
+    def set_goals(self, goal_locations):
+        self.goal_locations = [g for g in goal_locations]
+        return self
+    def set_obstacles(self, obstacle_locations=None, obstacle_prob=None, obstacle_random_state=None):
+        if obstacle_prob is None:
+            obstacle_prob = 0
+        if obstacle_locations is None:
+            obstacle_locations = []
+        obstacle_random_state = self.coalesce_random(obstacle_random_state)
+        self.obstacle_locations = [o for o in obstacle_locations]
+        # populate with random probs
+        rows, cols = self.grid.shape
+        for i in range(rows):
+            for j in range(cols):
+                if self.grid[i, j] != 0:
+                    continue
+                if obstacle_random_state.rand() < obstacle_prob:
+                    self.obstacle_locations.append(self.flatten(i, j))
+        return self
+    
+    # TODO: rotate?
+    #def rotate(self, )
+        
+    def build(self, transition_random_state=None, env_random_state=None):
+        for o in self.obstacle_locations:
+            row, col = self.row_col(o)
+            self.grid[row, col] = 1
+        for g in self.goal_locations:
+            row, col = self.row_col(g)
+            self.grid[row, col] = 2
+
+        G = gen.MDPGraph.from_grid(grid=self.grid, success_prob=self.success_prob, strat=self.strat)
+        G.R[G.R != 1] = self.step_reward
+        def add_noise(G, percent, random_state):
+            G = G.copy()
+            P = G.P
+            out_a = G.out_a
+            n_actions, n_states = P.shape
+            for i in range(n_actions):
+                for j in range(n_states):
+                    if P[i, j] <= 0:
+                        continue
+                    # Between [0, 1)
+                    noise = random_state.rand()
+                    # Between [-percent, +percent)
+                    z = -percent + 2*percent*noise
+                    # TODO: scale by element or nah? Probably?
+                    P[i, j] = max(0, P[i, j] + z)
+                normed_row = sk_norm(np.array([P[i]]), norm='l1')[0]
+                P[i, :] = normed_row
+                out_a[i] = normed_row.copy()
+            return G
+        transition_random_state = self.coalesce_random(transition_random_state)
+        if self.transition_noise > 0:
+            G = add_noise(G, self.transition_noise, transition_random_state)
+        env = MDPGraphEnv(graph=G, obs_size=self.obs_size, random_state=env_random_state)
+        return env
+    
+    # Utility functions
+    def row_col(self, state):
+        _, width = self.grid.shape
+        row = state // width
+        col = state - self.width*row
+        return row, col
+    def flatten(self, row, col):
+        _, width = self.grid.shape
+        return row*width + col
+    def coalesce_random(self, random_state=None):
+        return np.random if random_state is None else random_state
 
 RANDOM_SEED = 41239678
 RANDOM_STATE = np.random.RandomState(RANDOM_SEED)
-GRID = gen.create_grid((10, 10), obstacle_prob=0.2, random_state=RANDOM_STATE)
-G = gen.MDPGraph.from_grid(GRID, strat=gen.ActionStrategy.WRAP_NOOP_EFFECT)
-# TODO: randomize? Add noise later?
-G.R[G.R != 1] = -0.001
-ENV = MDPGraphEnv(G)
+# GRID = gen.create_grid((15, 15), obstacle_prob=0.2, random_state=RANDOM_STATE)
+# G = gen.MDPGraph.from_grid(GRID, strat=gen.ActionStrategy.WRAP_NOOP_EFFECT)
+# # TODO: randomize? Add noise later?
+# G.R[G.R != 1] = -0.001
+# ENV = MDPGraphEnv(G, obs_size=3)
+ENV = EnvironmentBuilder((15, 15)).set_obstacles(obstacle_prob=0.2, obstacle_random_state=RANDOM_STATE).build()
 
 def env_creator(_):
     return ENV # return an env instance
+
+# return mean, std, list and such
+def test_env(env: MDPGraphEnv, agent):
+    total_steps = 0
+    total_reward = 0
+    total_eps = 0
+    grid = env.grid
+    rows, cols = grid.shape
+    for i in range(rows):
+        for j in range(cols):
+            grid_entry = grid[i, j]
+            if grid_entry != 0:
+                continue
+            total_eps += 1
+            obs = env.reset(i*cols + j)
+            done = False
+            ep_reward = 0
+            state = agent.get_policy().model.get_initial_state()
+            step_count = 0
+            while not done:
+                step_count += 1
+                # TODO: is this stochastic? Can we make it determinstic?
+                action, state, _ = agent.compute_action(observation=obs, prev_action=0, prev_reward=0, state=state)
+                obs, reward, done, _ = env.step(action)
+                ep_reward += reward
+            total_steps += step_count
+            total_reward += ep_reward
+    return total_steps/total_eps, total_reward/total_eps
 
 # TODO:
 # - small negative rewards, when reward is 0?
@@ -34,15 +163,28 @@ def env_creator(_):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--test', help='test specified checkpoint')
+    parser.add_argument('--last', help='use last training session to resume training [NOT SUPPORTED] or test', action='store_true')
     parser.add_argument('--iters', default=100, help='number of iters to train')
     parser.add_argument('--algo', default='ppo', choices=['ppo', 'dqn'], help='which algorithm to train with')
     args = parser.parse_args()
     train = args.test is None
     iters = int(args.iters)
+    last = args.last
     algo = ppo if args.algo == 'ppo' else dqn
     algo_trainer = PPOTrainer if args.algo == 'ppo' else DQNTrainer
     register_env("gridworld", env_creator)
 
+    if last:
+        base_dir = os.path.expanduser('~/ray_results/gridworld')
+        def latest_file(path, dir=True):
+            files = [os.path.join(path, f) for f in os.listdir(path)]
+            return max([f for f in files if (os.path.isdir(f) if dir else os.path.isfile(f))], key=os.path.getctime)
+        latest_run = latest_file(base_dir)
+        latest_checkpoint_dir = latest_file(latest_run)
+        latest_checkpoint_files = [f for f in os.listdir(latest_checkpoint_dir) if 'checkpoint-' in f and '.' not in f]
+        assert len(latest_checkpoint_files) == 1, 'Could not find latest checkpoint file'
+        last_path = os.path.join(latest_checkpoint_dir, latest_checkpoint_files[0])
+    
     config = algo.DEFAULT_CONFIG.copy()
     print(config)
     config.update({"env": "gridworld",
@@ -52,36 +194,32 @@ if __name__ == '__main__':
             # },
 
             'framework':'torch',
-            "num_workers": 4,
+            "num_workers": 8,
             'model': {
-                    "use_lstm": False,
+                    #TODO: consider turning to True
+                    "use_lstm": True,
             }
             })
 
 
     if train:
-        tune.run(algo_trainer, config=config,
+        results = tune.run(algo_trainer, config=config,
                 checkpoint_freq = 10,
                 name="gridworld",
+                # TODO: use stopping condition of episodes_total for transfer learning?
                 stop={'training_iteration': iters}
         ) 
+        metric='training_iteration'
+        mode='max'
+        path = results.get_best_checkpoint(results.get_best_logdir(metric, mode), metric, mode)
+        print(path)
     else:
         ray.init()
         agent = algo_trainer(config=config, env='gridworld')
-        agent.restore(args.test)
+        restore_path = args.test if not last else last_path
+        print('Restoring from', restore_path)
+        agent.restore(args.test if not last else last_path)
 
-        done = False
-        ep_reward = 0
-        obs = ENV.reset()
-        print(obs)
-        step_count = 0
-        while not done:
-                step_count += 1
-                action = agent.compute_action(obs)
-                print(action)
-                obs, reward, done, _ = ENV.step(action)
-                print(obs)
-                ep_reward += reward
-        print(obs, ep_reward, step_count)
+        print(test_env(ENV, agent))
         import code; code.interact(local=vars())
 
