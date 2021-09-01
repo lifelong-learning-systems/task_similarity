bf9f26ed4f6c780ab3835c118008bd543fe20ee2
Ben Stoler <Benjamin.Stoler@jhuapl.edu>
2021-09-01 15:22:43 -0400

    first attempt at transfer

--- tasksim/qtrainer.py
+++ tasksim/qtrainer.py
@@ -1,5 +1,6 @@
+from numpy.core.fromnumeric import var
 import tasksim.gridworld_generator as gen
-from tasksim.train_environment import EnvironmentBuilder
+from tasksim.train_environment import EnvironmentBuilder, test_env
 from tasksim.environment import MDPGraphEnv
 import random
 import numpy as np
@@ -11,8 +12,7 @@ from os import path
 
 import argparse
 
-class QTrainer():
-
+class QTrainer:
     def __init__(self, env: MDPGraphEnv, agent_path, override_hyperparameters = True, lr=0.01, gamma=0.95, epsilon=1.0, min_epsilon=0.01, max_epsilon=1.0, decay=0.01, learning=True):
         self.env = env
         self.Q = None
@@ -64,6 +64,35 @@ class QTrainer():
         num_states = self.env.graph.grid.shape[0] * self.env.graph.grid.shape[1]
         self.Q = np.zeros((num_states, 4))
         
+    # Return new instance of QTrainer with transfered weights
+    # pass in S, A as distances/metrics
+    def transfer_to(self, other, direct_transfer=True, S=None, A=None):
+        if A is None or S is None:
+            S, A, _, _ = self.env.graph.compare(other.env.graph)
+        S = 1 - S
+        A = 1 - A
+        S_total = S.sum().sum()
+        A_total = A.sum().sum()
+        if not direct_transfer:
+            S = S / S_total
+            A = A / A_total
+        new_states, new_actions = other.Q.shape
+        other.Q = np.zeros(other.Q.shape)
+        old_states, old_actions = self.Q.shape
+        for s_i in range(new_states):
+            for a_i in range(new_actions):
+                for s_j in range(old_states):
+                    for a_j in range(old_actions):
+                        w_s = S[s_i, s_j]
+                        w_a = A[a_i, a_j]
+                        # TODO: determine transfer methodology: linear combine? State transfer only? State transfer argmax?
+                        if not direct_transfer:
+                            w = 0.5*w_s + 0.5*w_a
+                        else:
+                            # TODO: actually do 4.2 State Transfer from the paper
+                            w = 1 if S[s_i, :].argmax() == s_j else 0
+                        other.Q[s_i, a_i] += w*self.Q[s_j, a_j]
+
     def reset(self):
         self.episode += 1
         return self.env.reset()
@@ -152,15 +181,44 @@ if __name__ == '__main__':
             .set_obstacles(obstacle_prob=0.2, obstacle_random_state=np.random.RandomState(base_seed)) \
             .set_step_reward(-0.001) \
             .set_obs_size(7) \
-            .set_success_prob(1)\
+            .set_success_prob(0.9)\
+            .build()
+    env2 = EnvironmentBuilder((7, 7)) \
+            .set_obstacles(obstacle_prob=0.1, obstacle_random_state=np.random.RandomState(base_seed)) \
+            .set_step_reward(-0.001) \
+            .set_obs_size(7) \
+            .set_success_prob(0.7)\
+            .build()
+    env3 = EnvironmentBuilder((7, 7)) \
+            .set_obstacles(obstacle_prob=0.1, obstacle_random_state=np.random.RandomState(base_seed)) \
+            .set_goals([6*7+6]) \
+            .set_step_reward(-0.001) \
+            .set_obs_size(7) \
+            .set_success_prob(0.7)\
             .build()
     
     trainer = QTrainer(env, "agent1.json", learning=(not test))
     trainer.run(1000)
-    print(trainer.Q)
+
     plt.ion()
     plt.scatter([i for i in range(len(trainer.rewards))], trainer.rewards, 0.2)
     plt.show()
-    from tasksim.train_environment import test_env
     trainer = QTrainer(env, "agent1.json", learning=False)
-    print(test_env(env, trainer))
+    trainer2_baseline = QTrainer(env2, "agent2_baseline.json", learning=True)
+    trainer2_transfer = QTrainer(env2, "agent2_transfer.json", learning=True)
+    trainer.transfer_to(trainer2_transfer)
+    trainer3_baseline = QTrainer(env3, "agent3_baseline.json", learning=True)
+    trainer3_transfer = QTrainer(env3, "agent3_transfer.json", learning=True)
+    trainer.transfer_to(trainer3_transfer)
+    
+    #print('Baseline (Agent 1):', test_env(env, trainer))
+    num_eps = 200
+    trainer2_baseline.run(num_eps)
+    trainer2_transfer.run(num_eps)
+    trainer3_baseline.run(num_eps)
+    trainer3_transfer.run(num_eps)
+    print('Agent 2 baseline:', test_env(env2, trainer2_baseline))
+    print('Agent 2 transfer:', test_env(env2, trainer2_transfer))
+    print('Agent 3 baseline:', test_env(env3, trainer3_baseline))
+    print('Agent 3 transfer:', test_env(env3, trainer3_transfer))
+    import code; code.interact(local=vars())
